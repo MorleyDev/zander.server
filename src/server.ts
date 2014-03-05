bf25c8ba58d91f7b95b38a9b794442c3e1cabffe
MorleyDev <morleydev.co.uk@gmail.com>
2014-03-05 16:50:24 +0000

    Removed dependency on linq.js

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ function startServer() {
 
     function addController(path : string, controller) {
 
-        function createControllerRequestHandler(method : (r: model.HttpRequest) => model.HttpResponse) {
+         function createControllerRequestHandler(method : (r: model.HttpRequest) => model.HttpResponse) {
             return function (request, response, next) {
                 var httpRequest : model.HttpRequest = new model.HttpRequest();
                 httpRequest.headers = request.headers;
@@ -25,10 +25,9 @@ function startServer() {
             };
         }
 
-        require("linq")
-            .From(["get", "head", "post", "put", "delete", "trace", "options", "connect", "patch"])
-            .Where(function (x) { return controller[x] != null  })
-            .ForEach(function (x) { server[x](path, createControllerRequestHandler(controller[x])) });
+        ["get", "head", "post", "put", "delete", "trace", "options", "connect", "patch"]
+            .filter(function (x) { return controller[x] != null; })
+            .forEach(function (x) { server[x](path, createControllerRequestHandler(controller[x])) });
     }
 
     addController("/verify", new controller.VerifyController());
