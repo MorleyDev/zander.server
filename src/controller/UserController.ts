ee97debcd71da3035da389e71807fdfef237522d
MorleyDev <jason@morleydev.co.uk>
2014-04-25 16:09:09 +0100

    Added error message to User controller post

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -8,10 +8,13 @@ module controller {
     export class UserController {
         public post(request : model.HttpRequest, callback : (m : model.HttpResponse) => void) {
             var createUserDto:model.dto.CreateUserDto = request.body;
-            if (!validate.ValidateCreateUserDto(createUserDto))
-                callback(new model.HttpResponse(400, null));
-            else
-                callback(new model.HttpResponse(401, null));
+            try {
+                validate.ValidateCreateUserDto(createUserDto)
+            } catch(e) {
+                callback(new model.HttpResponse(400, { "Error" : e }));
+                return;
+            }
+            callback(new model.HttpResponse(401, { "Error" : "Unauthorized" }));
         }
         public put(request : model.HttpRequest, callback : (m : model.HttpResponse) => void) {
             callback(new model.HttpResponse(403, null));
--- src/validate/ValidateCreateUserDto.ts
+++ src/validate/ValidateCreateUserDto.ts
@@ -1,21 +1,19 @@
 /// <reference path="../model/dto/CreateUserDto.ts" />
 
 module validate {
-    export function ValidateCreateUserDto(dto : model.dto.CreateUserDto) : boolean {
+    export function ValidateCreateUserDto(dto : model.dto.CreateUserDto) {
         if (!dto.email)
-            return false;
+            throw "E-Mail Not Provided";
         if (!dto.username)
-            return false;
+            throw "Username Not Provided";
         if (!dto.password)
-            return false;
+            throw "Password Not Provided";
 
         if (dto.password.length < 3)
-            return false;
+            throw "Password Too Short";
         if (dto.username.length < 3 || dto.username.length > 20)
-            return false;
-        if (!dto.username.match("^[a-zA-Z0-9_]*$"))
-            return false;
-
-        return true;
+            throw "Username Not Between 3-20 Characters";
+        if (!dto.username.match("^[a-zA-Z0-9_\\-]*$"))
+            throw "Username Must Only Contain Alphanumeric Characters or Underscore";
     }
 }
