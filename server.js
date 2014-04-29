bda4ba2a0bac974d3e37f4eba39ccf00387e6566
MorleyDev <jason@morleydev.co.uk>
2014-04-29 14:53:56 +0100

    Fixed server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 function parseConfig(config) {
     config.host = process.env.zander_host || config.host || "127.0.0.1";
     config.port = process.env.zander_port || config.port || 1337;
@@ -9,10 +8,12 @@ function parseConfig(config) {
     config.goduser.name = process.env.zander_god_username || config.goduser.name;
     config.goduser.password = process.env.zander_god_password || config.goduser.password;
     config.hashAlgorithm = config.hashAlgorithm || "sha256";
+    config.throttle = config.throttle || { "burst" : 100, "rate" : 50, "ip" : true }
 
+    return config;
 }
-var config = parseConfig(require("./config.json"));
 
+var config = parseConfig(require("./config.json"));
 const zander = require("./lib/server.js");
 
     zander.bootstrapDatabase(config, function(err, database) {
